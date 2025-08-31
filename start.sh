b3b9847f04755d1d1bed0d2c81bf70f863cecfde
aspizu <aspizu@protonmail.com>
2025-09-01 01:14:29 +0530

    feat: add start script for frontend and backend setup

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+set -euo pipefail
+git pull
+cd ~/confessionrooms/frontend
+bun install
+mkdir -p /var/www/html/confessionrooms
+bun run build --outDir /var/www/html/confessionrooms
+chown -R www-data:www-data /var/www/html/confessionrooms
+chmod -R 755 /var/www/html/confessionrooms
+cd ..
+cd backend
+set +e
+kill -9 "$(< /tmps/confessionrooms_backend.pid)"
+set -e
+uv sync
+uv pip install -e .
+uv run python -m gunicorn src.thesocialnetwork:app \
+  -k uvicorn.workers.UvicornWorker \
+  -b unix:/tmp/confessionrooms_backend.sock \
+  --log-file /var/log/gunicorn.log \
+  --pid /tmp/confessionrooms_backend.pid > /dev/null 2>&1 & disown
